bc4338c84decab2aa6dae133c683ed5f88a4f988
zachnitzkin <znitz23@gmail.com>
2023-05-16 12:35:47 -0500

    wrote create table and drop table functions before paring off

--- server/db/seed.js
+++ server/db/seed.js
@@ -4,7 +4,13 @@ const dropTables = async () => {
   try {
     console.log('Starting to drop all tables...');
     await client.query(`
-    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS genres;
+    DROP TABLE IF EXISTS gameGenres;
+    DROP TABLE IF EXISTS systems;
+    DROP TABLE IF EXISTS orderItems;
+    DROP TABLE IF EXISTS orders;
+    DROP TABLE IF EXISTS products;
+    DROP TABLE IF EXISTS customers;
     `);
     console.log('Finished droppping all tables successfully!');
   } catch (error) {
@@ -13,15 +19,56 @@ const dropTables = async () => {
   }
 };
 
-const createTables = async () => {
+async function createTables() {
+  console.log("Starting to build tables...")
+  // create all tables, in the correct order
   try {
-    console.log('Starting to create all tables...');
     await client.query(`
-    CREATE TABLE users(
-      id SERIAL PRIMARY KEY,
-      username VARCHAR(255) UNIQUE NOT NULL,
-      password VARCHAR(255) NOT NULL
+    CREATE TABLE customers (
+      id SERIAL primary key,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(75) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL,
+      address TEXT NOT NULL,
+      admin BOOLEAN DEFAULT false
     );
+
+    CREATE TABLE orders (
+      id SERIAL primary key,
+      orderCompleted BOOLEAN DEFAULT false,
+      "customerId" REFERENCES customers(id)
+    );
+
+    CREATE TABLE products (
+      id SERIAL primary key,
+      name VARCHAR(255) NOT NULL,
+      price MONEY NOT NULL,
+      description TEXT,
+      imageUrl TEXT NOT NULL
+      inventory NUM NOT NULL
+    );
+
+    CREATE TABLE system(
+      id SERIAL primary key,
+      name VARCHAR(20) UNIQUE NOT NULL
+    );
+
+    CREATE TABLE genre(
+      id SERIAL primary key,
+      name VARCHAR(20) UNIQUE NOT NULL
+    );
+
+    CREATE TABLE gameGeneres(
+      "productId" REFERNECES products(id),
+      "genreId" REFERNECES genre(id)
+    )
+
+    CREATE TABLE orderItems(
+      orderItemId SERIAL PRIMARY KEY,
+      "productId" REFERENCES products(id),
+      quantity NUM,
+      purchasePrice NUM NOT NULL
+    )
     `);
     console.log(
       'Finished creating all tables successfully! Now, to add some data!'
