156701fcf653287d3fd7084873e6bc8bc03a4132
Evan Walker <ewalker3764@gmail.com>
2023-05-17 13:04:00 -0400

    createGame1

--- server/db/products.js
+++ server/db/products.js
@@ -1,23 +1,108 @@
 const client = require("./client");
 
-const getAllGames = async () => {};
+const getAllGames = async () => {
+  try {
+    const { rows } = await client.query(`
+  SELECT *
+  FROM games;
+`);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+};
 
-const getAllConsoles = async () => {};
+const getAllConsoles = async () => {
+  try {
+    const { rows } = await client.query(`
+  SELECT *
+  FROM consoles;
+`);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+};
 
-const getConsoleById = async (id) => {};
+const getConsoleById = async (id) => {
+  try {
+    const {
+      rows: [console],
+    } = await client.query(
+      `
+        SELECT *
+        FROM console
+        WHERE id=$1;
+        `,
+      [id]
+    );
+    return console;
+  } catch (error) {
+    throw error;
+  }
+};
 
-const getGameByName = async (name) => {};
+const getGameByName = async (name) => {
+  try {
+    const {
+      rows: [games],
+    } = await client.query(
+      `
+        SELECT *
+        FROM games
+        WHERE name=$1;
+        `,
+      [name]
+    );
+    return games;
+  } catch (error) {
+    throw error;
+  }
+};
 
-const getConsoleByName = async (name) => {};
+const getConsoleByName = async (name) => {
+  try {
+    const {
+      rows: [console],
+    } = await client.query(
+      `
+        SELECT *
+        FROM console
+        WHERE name=$1;
+        `,
+      [name]
+    );
+    return console;
+  } catch (error) {
+    throw error;
+  }
+};
 
-const createListing = async ({
+const createGame = async ({
   name,
   price,
   description,
-  url,
+  imageUrl,
   inventory,
-  ...systemId
-}) => {};
+}) => {
+  try {
+    const {
+      rows: [game],
+    } = await client.query(
+      `
+        INSERT INTO product( name, price, description, imageUrl, inventory)
+        VALUES ($1, $2, $3, $4, $5)
+        ON CONFLICT (name) DO NOTHING
+        RETURNING *;
+        `,
+      [name, price, description, imageUrl, inventory]
+    );
+    return game;
+  } catch (error) {
+    throw error;
+    console.log(error);
+  }
+};
 
-const updateListing = async ({ id, ...fields }) => {};
-const deleteListing = async (id) => {};
+const updateGame = async ({ id, ...fields }) => {};
+const deleteGame = async (id) => {};
