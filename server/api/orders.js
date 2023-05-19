68e5f2bb8ecf5533d6602e750ec00e269f386fe7
Peter Laughlin <pwlaughlin@gmail.com>
2023-05-19 12:10:23 -0500

    Orders database complete

---
+++ server/api/orders.js
@@ -0,0 +1,28 @@
+// const express = require("express");
+// const { get, post } = require(".");
+// const { getAllOrdersByCustomer } = require("../db/orders");
+// const ordersRouter = express.Router();
+
+// ordersRouter.get('/', async (req, res, next) => {
+//     const { user: email} = req.body;
+//     try {
+//     const orders = await getAllOrdersByCustomer(email);
+// console.log(orders)
+//     res.send(orders)
+// } catch (error) {
+//     next(error)
+// }
+
+// })
+
+
+
+
+
+// post
+
+
+
+
+
+
