daa806c56a89a87fa2620088543758ae898c03bf
zachnitzkin <znitz23@gmail.com>
2023-05-20 09:05:33 -0500

    secondary commit, added use effect to check for existing token

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import NavBar from './components/NavBar'
+import { getMe } from './api/customers'
 
 function App() {
   const [token, setToken] = useState("")
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [customer, setCustomer] = useState([])
+
+  useEffect(() => {
+    const getInitialData = async () => {
+        if(token) {
+            const me = await getMe(token);
+            setUser(me);
+            setIsLoggedIn(true)
+        }
+    }
+    getInitialData();
+}, [])
 
   return (
     <div className="App">
