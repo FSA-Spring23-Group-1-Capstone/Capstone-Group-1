7f0b7c3067f9855eae4bdd25b5ed34bebea12b8f
zachnitzkin <znitz23@gmail.com>
2023-05-23 09:07:41 -0500

    added create game form and api fetch

---
+++ client/src/components/CreateGameForm.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { createGame } from "../api/games";
+
+const CreateGameForm = ({token}) => {
+    console.log('this is token:' , token)
+    const [name, setName] = useState("")
+    const [description, setDescription] = useState("")
+    const [imageUrl, setImageUrl] = useState("")
+    const [price, setPrice] = useState("")
+    const [inventory, setInventory] = useState("")
+    const [system, setSystem] = useState("")
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log('this is sytem: ', system)
+        const newGame = await createGame(name, price, description, imageUrl, inventory, system, token);
+        console.log('newgame:', newGame)
+            setName("")
+            setDescription("")
+            setImageUrl("")
+            setPrice("")
+            setInventory("")
+            setSystem("")
+
+    }
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Name" value={name} onChange={(event) => {setName(event.target.value)}}></input>
+                <input type="text" placeholder="Description" value={description} onChange={(event) => setDescription(event.target.value)}></input>
+                <input type="text" placeholder="Image URL" value={imageUrl} onChange={(event) => setImageUrl(event.target.value)}></input>
+                <input type="text" placeholder="Price" value={price} onChange={(event) => setPrice(event.target.value)}></input>
+                <input type="text" placeholder="inventory" value={inventory} onChange={(event) => setInventory(event.target.value)}></input>
+                <select name="system" value={system} onChange={(event) => {setSystem(event.target.value)}}>
+                    <option>Select System</option>
+                    <option value="Xbox">Xbox</option>
+                    <option value="Playstation">Playstation</option>
+                    <option value="Nintendo">Nintendo</option>
+                </select>
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    )
+}
+
+export default CreateGameForm
