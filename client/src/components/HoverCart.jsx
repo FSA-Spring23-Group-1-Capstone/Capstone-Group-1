46541cd790bc498b29e43fee16edafa57a9d81c0
Christian McNeil <christianmcneil@christiansmbp2.mynetworksettings.com>
2023-05-24 19:51:34 -0400

    add items to cart

--- client/src/components/HoverCart.jsx
+++ client/src/components/HoverCart.jsx
@@ -3,13 +3,13 @@ import { getAllOrderItemsByOrderId } from "../api/orderItems";
 import { ordersByCustomerEmail } from "../api/orders";
 import { getGameByGameId } from "../api/games";
 
-
 const HoverCart = ({ customer, token, allGames }) => {
   const [allItemsOrders, setAllItemsOrders] = useState([]);
   const [showCart, setShowCart] = useState(false);
-
+  const [cartItems, setCartItems] = useState([]);
   const orderIdByCustomerEmail = async () => {
     const result = await ordersByCustomerEmail(customer.email);
+
     return result.id;
   };
 
@@ -22,26 +22,46 @@ const HoverCart = ({ customer, token, allGames }) => {
     setAllItemsOrders(result);
   };
 
-  console.log("All items order: ", allItemsOrders);
- 
-  const customerCart = allItemsOrders.forEach( async (order)=> {
-    await getGameByGameId(order.productId)
-    
-  })
-  console.log(customerCart)
+  const getAllGames = async () => {
+    let cartItem = await Promise.all(
+      allItemsOrders.map(
+        async (order) => await getGameByGameId(order.productId, token)
+      )
+    );
+    return cartItem;
+  };
+  const extractGames = async () => {
+    setCartItems(await getAllGames());
+  };
 
-  
+  console.log(allItemsOrders);
+
   return (
     <div>
       <button
         onClick={() => {
+          extractGames();
           getAllOrderItems();
           setShowCart(!showCart);
         }}
       >
         <i className="fa-solid fa-cart-shopping"></i>
       </button>
-      
+      {showCart ? (
+        <div>
+          {cartItems.map((game, index) => {
+            return (
+              <div key={game.id}>
+                <h4>Name: {game.name}</h4>
+                <h5>Price: {game.price}</h5>
+                <p>Quantity: {allItemsOrders[index].quantity}</p>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
