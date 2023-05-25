5824c2734a643e699cea21c77aee6e1858a5e0e0
Christian McNeil <christianmcneil@christiansmbp2.mynetworksettings.com>
2023-05-25 11:14:42 -0400

    Dynamically add to cart

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -1,58 +1,57 @@
-
 import React, { useEffect, useState } from "react";
 import Cart from "./Cart";
 import { ordersByCustomerEmail } from "../api/orders";
 import { getAllOrderItemsByOrderId } from "../api/orderItems";
 
-const Checkout = ({customer, token, allGames}) => {
+const Checkout = ({ customer, token, allGames }) => {
   const [orderItems, setOrderItems] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     const getCustomerOrder = async () => {
-      const customerOrder = await ordersByCustomerEmail(customer.email)
-      const fetchedOrderItems = await getAllOrderItemsByOrderId(customerOrder.id, token)
-      setOrderItems(fetchedOrderItems)
-    }
-  
-    getCustomerOrder()
-  },[])
+      const customerOrder = await ordersByCustomerEmail(customer.email);
+      const fetchedOrderItems = await getAllOrderItemsByOrderId(
+        customerOrder.id,
+        token
+      );
+      setOrderItems(fetchedOrderItems);
+    };
 
+    getCustomerOrder();
+  }, []);
 
   return (
-  <>
-    <div>
-      <h2>Checkout</h2>
-    </div>
-    <div>
-      <section className="userCheckoutInfo">
-        <h3>Account Details</h3>
-        <p>Name: {customer.name}</p>
-        <p>Name: {customer.email}</p>
-        <p>Address: {customer.address}</p>
-      </section>
-    </div>
-    <div>
+    <>
+      <div>
+        <h2>Checkout</h2>
+      </div>
+      <div>
+        <section className="userCheckoutInfo">
+          <h3>Account Details</h3>
+          <p>Name: {customer.name}</p>
+          <p>Name: {customer.email}</p>
+          <p>Address: {customer.address}</p>
+        </section>
+      </div>
+      <div>
         <h3>Cart Items</h3>
-      <section className="checkoutItems">
-        {orderItems.map((item) => {
-          const currentGame = allGames.filter((game) => game.id === item.productId)
-          return (
-            <article className="checkoutItem">
-              <img src={currentGame[0].imageUrl} alt={currentGame[0].name} />
-              <p>{currentGame[0].name}</p>
-              <p>{currentGame[0].price}</p>
-            </article>
-          )
-        })}
-
-      </section>
-    </div>
-
-  
-  
-  </>
-  )
-
+        <section className="checkoutItems">
+          {orderItems.map((item, index) => {
+            const currentGame = allGames.filter(
+              (game) => game.id === item.productId
+            );
+            return (
+              <article className="checkoutItem" key={currentGame[0].id}>
+                <img src={currentGame[0].imageUrl} alt={currentGame[0].name} />
+                <p>{currentGame[0].name}</p>
+                <p>{currentGame[0].price}</p>
+                <p>{orderItems[index].quantity}</p>
+              </article>
+            );
+          })}
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default Checkout;
