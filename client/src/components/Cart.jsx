f6711e32f33fae99fcf16eee970fe0bd930422a0
Evan Walker <ewalker3764@gmail.com>
2023-05-31 10:39:12 -0400

    some final css

--- client/src/components/Cart.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Cart = () => {
-  return <div style={{ backgroundColor: "black" }}>CartTttttttttttt</div>;
-};
-
-export default Cart;
--- client/src/components/CheckoutForm.jsx
+++ client/src/components/CheckoutForm.jsx
@@ -1,33 +1,46 @@
 import React from "react";
-import { createNewCustomerOrder, ordersByCustomerEmail, setOrderCompleted } from "../api/orders";
-
-const CheckoutForm = ({customer, orderId, setOrderItems, setOrderId}) => {
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const completedOrder = await setOrderCompleted(orderId)
-        setOrderItems([])
-        console.log("LLLL", completedOrder)
-        if(completedOrder.orderCompleted){
-            const newCart = await createNewCustomerOrder(customer.id);
-            const customerOrders = await ordersByCustomerEmail(customer.email);
-            const cartNum = customerOrders.filter((order) => order.orderCompleted === false)
-            console.log("new cart", cartNum)
-            setOrderId(cartNum[0].id)
-            setOrderItems([])
-        }
-        return completedOrder
+import {
+  createNewCustomerOrder,
+  ordersByCustomerEmail,
+  setOrderCompleted,
+} from "../api/orders";
+const CheckoutForm = ({ customer, orderId, setOrderItems, setOrderId }) => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const completedOrder = await setOrderCompleted(orderId);
+    setOrderItems([]);
+    console.log("LLLL", completedOrder);
+    if (completedOrder.orderCompleted) {
+      const newCart = await createNewCustomerOrder(customer.id);
+      const customerOrders = await ordersByCustomerEmail(customer.email);
+      const cartNum = customerOrders.filter(
+        (order) => order.orderCompleted === false
+      );
+      console.log("new cart", cartNum);
+      setOrderId(cartNum[0].id);
+      setOrderItems([]);
     }
-return (
+    return completedOrder;
+  };
+  return (
     <>
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Name"></input>
-            <input type="text" placeholder="Card Number"></input>
-            <input type="text" placeholder="Security Code"></input>
-            <button type="submit">Checkout</button>
-        </form>
+      <form className="check-form" onSubmit={handleSubmit}>
+        <input className="form-inputs" type="text" placeholder="Name"></input>
+        <input
+          className="form-inputs"
+          type="text"
+          placeholder="Card Number"
+        ></input>
+        <input
+          className="form-inputs"
+          type="text"
+          placeholder="Security Code"
+        ></input>
+        <button className="checkout-button" type="submit">
+          Checkout
+        </button>
+      </form>
     </>
-)
-}
-
-export default CheckoutForm
+  );
+};
+export default CheckoutForm;
