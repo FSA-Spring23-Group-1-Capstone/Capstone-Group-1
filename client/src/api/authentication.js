8b21ead9507c26b11045c34eb249220ff304c97d
Christian McNeil <christianmcneil@christiansmbp2.mynetworksettings.com>
2023-05-31 15:30:46 -0400

    Revised

--- client/src/api/authentication.js
+++ client/src/api/authentication.js
@@ -1,53 +1,57 @@
-const APIURL = 'api'
+const APIURL = "api";
 
-export const authenticateNewCustomer = async ({name, email, password, address}) => {
-    console.log('RRRRR', email, password)
-    try {
-        const response  = await fetch(`${APIURL}/customer/register`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({name, email, password, address})
-        })
+export const authenticateNewCustomer = async ({
+  name,
+  email,
+  password,
+  address,
+}) => {
+  try {
+    const response = await fetch(`${APIURL}/customer/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password, address }),
+    });
 
-        const result = await response.json();
-        console.log('*****', result)
-        const {token, customer, message} = result;
-        if(token){
-            localStorage.setItem('token', token)
-            return {customer, token, message}
-        }
-        if(!token){
-            return {message}
-        }
-        return
-    } catch (error) {
-        console.error(error)
+    const result = await response.json();
+
+    const { token, customer, message } = result;
+    if (token) {
+      localStorage.setItem("token", token);
+      return { customer, token, message };
     }
-}
+    if (!token) {
+      return { message };
+    }
+    return;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const authenticateCustomer = async (email, password) => {
-    try {
-        const response  = await fetch(`${APIURL}/customer/login`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({email, password})
-        })
+  try {
+    const response = await fetch(`${APIURL}/customer/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
 
-        const result = await response.json();
-        const {token, customer, message} = result;
-        if(token){
-            localStorage.setItem('token', token)
-            return {customer, token, message}
-        }
-        if(!token){
-            return {message}
-        }
-        return
-    } catch (error) {
-        console.error(error)
+    const result = await response.json();
+    const { token, customer, message } = result;
+    if (token) {
+      localStorage.setItem("token", token);
+      return { customer, token, message };
     }
-}
+    if (!token) {
+      return { message };
+    }
+    return;
+  } catch (error) {
+    console.error(error);
+  }
+};
